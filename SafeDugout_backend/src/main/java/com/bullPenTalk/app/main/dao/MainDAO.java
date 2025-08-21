141f88a67be1919cd2dc06898d33b2ce15031020
bjy5656 <qorwjddl1234@gmail.com>
2025-08-21 11:16:34 +0900

    250821 백정이 작업 저장

--- SafeDugout_backend/src/main/java/com/bullPenTalk/app/main/dao/MainDAO.java
+++ SafeDugout_backend/src/main/java/com/bullPenTalk/app/main/dao/MainDAO.java
@@ -2,9 +2,10 @@ package com.bullPenTalk.app.main.dao;
 
 import org.apache.ibatis.session.SqlSession;
 
-import com.bullPenTalk.app.dto.MemberDTO;
+import com.bullPenTalk.app.dto.MainNoticePostDTO;
 import com.bullPenTalk.config.MyBatisConfig;
 
+//메인페이지 DAO
 public class MainDAO {
 	public SqlSession sqlSession;
 	
@@ -12,9 +13,26 @@ public class MainDAO {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	}
 	
-	public void selectMainList(MemberDTO memberDTO) {
-		sqlSession.insert("member.join", memberDTO);
+//	public void selectMainList(MemberDTO memberDTO) {
+//		sqlSession.insert("member.join", memberDTO);
+//	}
+	
+	//메인 공지사항 목록 조회
+	public MainNoticePostDTO selectMainList() {
+		sqlSession.insert("main.selectMainlist");
 	}
 	
+	//메인 공지사항 상세 조회
+	public MainNoticePostDTO selectMainList() {
+		sqlSession.insert("main.selectMainDetail");
+	}
 	
+	//경기 일정 목록 조회
+	
+	
+	//중고거래 목록 조회
+	
+	//입문가이드 목록 조회
+	
+	//팀별 순위 조회
 }
