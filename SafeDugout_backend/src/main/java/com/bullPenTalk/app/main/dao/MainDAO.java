30cc27ee25cea80afef0b4f755e030b33f68e589
bjy5656 <qorwjddl1234@gmail.com>
2025-08-21 09:48:31 +0900

    250821 백정이 작업 저장

---
+++ SafeDugout_backend/src/main/java/com/bullPenTalk/app/main/dao/MainDAO.java
@@ -0,0 +1,19 @@
+package com.bullPenTalk.app.main.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.bullPenTalk.app.dto.MemberDTO;
+import com.bullPenTalk.config.MyBatisConfig;
+
+public class MainDAO {
+	public SqlSession sqlSession;
+	
+	public MainDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	
+	public void (MemberDTO memberDTO) {
+		sqlSession.insert("member.join", memberDTO);
+	}
+	
+}
