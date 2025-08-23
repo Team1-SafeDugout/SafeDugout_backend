0162bb18e6de4153e2af8b5228db40cc53b41b32
jsj4653 <sungjae465377@gmail.com>
2025-08-23 16:46:51 +0900

    250823 조성재 마이페이지 컨트롤러

--- SafeDugout_backend/src/main/java/com/bullPenTalk/app/myPage/CommentDeleteOkController.java
+++ SafeDugout_backend/src/main/java/com/bullPenTalk/app/myPage/CommentDeleteOkController.java
@@ -1,5 +1,11 @@
 package com.bullPenTalk.app.myPage;
 
-public class CommentDeleteOkController {
-
+public class CommentDeleteOkController implements Execute {
+	@Override
+	public Result execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
