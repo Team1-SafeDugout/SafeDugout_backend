f27022fe5d30a1303fdef495ffcd51397f874ef4
LeeTaeHo00 <th3179@naver.com>
2025-08-26 17:57:10 +0900

    250825 이태호 회원목록 조회

--- SafeDugout_backend/src/main/webapp/assets/js/admin/adminMenu/adminManageMember.js
+++ SafeDugout_backend/src/main/webapp/assets/js/admin/adminMenu/adminManageMember.js
@@ -1,76 +1,14 @@
 const userList = document.getElementById("list-ul");
-const addbutton = document.getElementsByClassName("list-names").item(0);
 
-const leftButton = document.getElementById("left-button");
-const rightButton = document.getElementById("right-button");
-const numberList = document.querySelectorAll("#numberlist-ul>li");
-
-const userNums = 12;
-var curPage = 0;
-
-// 숫자 버튼들 세팅
-function setNumber() {
-  for (var i = 0; i < numberList.length; i++) {
-    var curNum = i + (Math.floor((curPage / 5)) * 5);
-    numberList.item(i).firstChild.firstChild.nodeValue = curNum + 1;
-
-    if (curPage == curNum) {
-      numberList.item(i).style.backgroundColor = "#4D5061";
-      numberList.item(i).style.color = "white";
-    }
-
-    else {
-      numberList.item(i).style.backgroundColor = "white";
-      numberList.item(i).style.color = "black";
-    }
-
-    if (curNum > userNums) {
-      numberList.item(i).style.display = "none";
-    }
-    else {
-      numberList.item(i).style.display = "block";
-    }
-  }
-}
-
-
-// leftright button 보이기 말기
-function setButton() {
-  if (curPage == 0) {
-    leftButton.style.display = "none";
-  } else {
-    leftButton.style.display = "block";
-  }
-
-  if (curPage >= userNums) {
-    rightButton.style.display = "none";
-  } else {
-    rightButton.style.display = "block";
-  }
-}
-
-// 왼쪽 오른쪽 버튼 보이기 말기
-leftButton.addEventListener('click', function () {
-  if (curPage > 0) {
-    curPage--;
-    setNumber();
-  }
-  setButton();
+document.addEventListener("DOMContentLoaded", function() {
+	if (typeof posts !== 'undefined') {
+		posts.forEach(post => {
+			addPost(post.number, post.id, post.date);
+		});
+	}
 });
-
-// 테스트용 왼쪽 오른쪽 버튼
-rightButton.addEventListener('click', function () {
-  if (curPage < userNums) {
-    curPage++;
-    setNumber();
-  }
-  setButton();
-});
-
 // 테스트용 추가 버튼
-addbutton.addEventListener('click', function () {
-
-  setNumber();
+function addPost(userNumberParam, userIdParam, addDateParam){
   const userListNum = document.querySelectorAll("#list-ul>li");
   if (userListNum.length >= 10) return;
 
@@ -83,11 +21,11 @@ addbutton.addEventListener('click', function () {
   const userIdURL = document.createElement('a');
   userID.appendChild(userIdURL);
 
-  const userNumberText = document.createTextNode("userNumber");
-  const userIDText = document.createTextNode("userID");
-  const addDateText = document.createTextNode("addDate");
+  const userNumberText = document.createTextNode(userNumberParam);
+  const userIDText = document.createTextNode(userIdParam);
+  const addDateText = document.createTextNode(addDateParam);
 
-  userIdURL.setAttribute('href', "./../adminDetailMenu/adminUserDetail.html");
+/*  userIdURL.setAttribute('href', "./../adminDetailMenu/adminUserDetail.html");*/
 
   userNumber.appendChild(userNumberText);
   userID.appendChild(userIdURL);
@@ -99,11 +37,4 @@ addbutton.addEventListener('click', function () {
   newMember.appendChild(addDate);
 
   userList.appendChild(newMember);
-});
-
-
-
-window.onload = function () {
-  setNumber();
-  setButton();
-};
+}
