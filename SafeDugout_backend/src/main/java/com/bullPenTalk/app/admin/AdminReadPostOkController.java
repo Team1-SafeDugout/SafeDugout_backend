755038f5c24419773fef6343a41e10553cbc1486
LeeTaeHo00 <th3179@naver.com>
2025-08-27 16:32:19 +0900

    250827 이태호 포스트 읽기 작업

--- SafeDugout_backend/src/main/java/com/bullPenTalk/app/admin/AdminReadPostOkController.java
+++ SafeDugout_backend/src/main/java/com/bullPenTalk/app/admin/AdminReadPostOkController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.bullPenTalk.app.Execute;
 import com.bullPenTalk.app.Result;
+import com.bullPenTalk.app.admin.dao.AdminMemberDAO;
 import com.bullPenTalk.app.admin.dao.AdminTeamNewsDAO;
 import com.bullPenTalk.app.admin.dao.AdminTeamYoutubeDAO;
 import com.bullPenTalk.app.admin.dao.TeamCommentDAO;
@@ -33,7 +34,7 @@ public class AdminReadPostOkController implements Execute{
 		switch(boardId) {
 		case 1:
 //			전체 커뮤니티
-			System.out.println("뉴스여는중");
+			System.out.println("전체게시판 여는중");
 			FreePostDAO freePostDAO = new FreePostDAO();
 			FreePostDTO freePostDTO = freePostDAO.detailSelect(postNumber);
 			result.setPath("/app/admin/adminDetailMenu/adminFreeComunityUserPost.jsp");
@@ -43,11 +44,11 @@ public class AdminReadPostOkController implements Execute{
 			
 		case 2:
 //			팀 커뮤니티 
-			System.out.println("뉴스여는중");
-			TeamCommunityDAO teamPostDAO = new TeamCommunityDAO();
-			PostDetailDTO teamPostDTO = teamPostDAO.postDetail(postNumber);
+			System.out.println("팀 게시판 여는중");
+			/* AdminMemberDAO teamPostDAO = new TeamCommunityDAO(); */
+			/* PostDetailDTO teamPostDTO = teamPostDAO.postDetail(postNumber); */
 			result.setPath("/app/admin/adminDetailMenu/adminFreeComunityUserPost.jsp");
-			request.setAttribute("teamPostDTO", teamPostDTO);
+			request.setAttribute("teamPostDTO", 1);
 			result.setRedirect(false);
 			break;
 			
